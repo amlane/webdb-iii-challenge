77b1c2de84ebbd4b733e9691a3e6a08f4051d4af
amlane <twelvexstring@gmail.com>
2019-06-05 15:24:14 -0500

    all end points tested for get, post, getbyid, put and delete cohorts

--- data/migrations/20190605150750_cohorts_table.js
+++ data/migrations/20190605150750_cohorts_table.js
@@ -1,11 +1,12 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('cohorts', function(tbl){
+      // primary key: id, autoincrements, integer
       tbl.increments();
 
+      //name: text, required, 128 character limit just cuz
       tbl.string('name', 128)
       .notNullable()
-      .unique();
   })
 };
 
